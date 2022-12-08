433a8f6058522199fef0daf4262b7cfd7e4c7281
Jesse Siver <123JSiver@frhsd.com>
2022-12-08 08:35:33 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+const getTodos = (resource, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if(request.readyState === 4 && request.status === 200){
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data);
+        } else if(request.readyState === 4){
+            callback('could not fetch data', undefined);
+        }
+    });
+
+    request.open('GET', resource);
+    request.send();
+};
+
+// getTodos('todos.json', (err, data) => {
+// console.log(data);
+// getTodos('jesse.json', (err, data) => {
+//     console.log(data);
+//     getTodos('sussy.json', (err, data) => {
+//         console.log(data);
+//     });
+// });
+// });
+
+const getSomething = () => {
+
+return new Promise((resolve, reject) => {
+    //fetch something
+    // resolve('some data');
+    reject('some error');
+});
+
+};
+
+// getSomething().then((data) => {
+// console.log(data);
+// }, (err) => {
+
+// });
+
+getSomething().then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
